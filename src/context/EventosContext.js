37bed74b6d080bf4ef09feb79b4f679d7d9650a2
Edson Amaya <edsonamaya@Hugos-iMac.local>
2019-08-13 21:11:16 -0500

    first commit

---
+++ src/context/EventosContext.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const EventosContext = React.createContext();
+export const EventosConsumer = EventosContext.Consumer;
+
+class EventosProvider extends Component {
+
+    token = 'DFTGHGIRZOGP4ZEXRQSM';
+    ordenar = 'date';
+
+    state = {
+        eventos : []
+    }
+
+    obtenerEventos = async (busqueda) => {
+        let url = `https://www.eventbriteapi.com/v3/events/search/?q=${busqueda.nombre}
+                    &categories=${busqueda.categoria}&sort_by=${this.date}
+                    &token=${this.token}&locale=es_ES`;
+
+        let eventos = await axios.get(url);
+        //console.log(eventos.data.events);
+        this.setState({
+            eventos : eventos.data.events
+        });
+
+    }
+
+    render() {
+        return ( 
+            <EventosContext.Provider
+                value={{
+                    eventos: this.state.eventos,
+                    obtenerEventos : this.obtenerEventos
+                }}
+            >
+                {this.props.children}
+            </EventosContext.Provider>
+        );
+    }
+}
+
+export default EventosProvider;
